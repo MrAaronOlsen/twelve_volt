33e56bb65218bf20211aa7059a01ee79b8fd7c9c
Aaron Olsen <mraaronolsen@gmail.com>
2017-07-10 08:20:24 -0600

    Sketched out Particle Contact

--- lib/p_contact.rb
+++ lib/p_contact.rb
@@ -1,9 +1,10 @@
 class PContact
 
   def initialize(particle1, particle2, restitution, contact_normal)
-    @particle = [ first: particle1, second: particle2 ]
+    @particle = { first: particle1, second: particle2 }
     @restitution = restitution
     @contact_normal = contact_normal
+    @penetration = penetration
   end
 
   def resolve(duration)
@@ -20,10 +21,45 @@ class PContact
     end
   end
 
+  private
+
   def resolve_velocity(duration)
     seperating_velocity = calculate_seperating_velocity
 
-    unless seperating_velocity > 0
-      new_seperating_velocity = seperating_velocity * restitution
+    return if seperating_velocity > 0
 
+    new_seperating_velocity = seperating_velocity.inverse * restitution
+    delta_velocity = new_seperating_velocity - seperating_velocity
+
+    total_i_mass = particle[:first].inverse_mass
+    total_i_mass.add!(particle[:second].inverse_mass) if @particle[:second]
+
+    return if total_inverse_mass <= 0
+
+    impulse = delta_velocity / total_i_mass
+    impulse_per_i_mass = @contact_normal * impulse
+
+    particle[:first].velocity.add!( impulse_per_i_mass * particle[:first].inverse_mass )
+
+    if particle[:second]
+      particle[:second].velocity.add!( impulse_per_i_mass * -particle[:second].inverse_mass )
+    end
+  end
+
+  def resolve_interpenetration(duration)
+    return if @penetration <= 0
+
+    total_i_mass = particle[:first].inverse_mass
+    total_i_mass.add!(particle[:second].inverse_mass) if particle[:second]
+
+    return if total_i_mass <= 0
+
+    move_per_i_mass = @contact_normal * (@penetration / total_i_mass)
+
+    p_mov_first = move_per_i_mass * particle[:first].inverse_mass
+    p_mov_sec = move_per_i_mass * particle[:second].inverse_mass if particle[:second]
+
+    particle[:first].position.add!(p_mov_first)
+    particle[:second].position.add!(p_mov_sec) if particle[:second]
+  end
 end
